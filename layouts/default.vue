329d173ba056a951b69b140ddfd38db108c10f70
Moses Kamau <moseskamau338@gmail.com>
2023-11-03 10:33:40 +0300

    added base lines and layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos et excepturi exercitationem facere incidunt, ipsa ipsum nisi ullam voluptates? A aspernatur et expedita explicabo modi non omnis possimus quam veniam!
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
