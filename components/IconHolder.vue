29d89fb54db103d7ce0068549171f0f2ef2f0186
Moses Kamau <moseskamau338@gmail.com>
2023-12-01 16:13:25 +0300

    hero design in progress

---
+++ components/IconHolder.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="relative" :style="`filter: drop-shadow(8px 8px 16px ${props.shadow});`">
+      <span class="absolute grow-0 shrink-0 rounded-full -translate-x-1 -translate-y-1 icon-holder-bg" :class="size" :style="`background: linear-gradient(180deg, ${props.gradientStops.start} 0%, ${props.gradientStops.stop} 100%);`" />
+      <span class="-translate-x-3 translate-y-1 absolute grid place-content-center grow-0 shrink-0 rounded-full glass-icon-holder text-black" :class="size">
+        <slot>K</slot>
+      </span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  gradientStops:{type:Object, default:{start:'#EC7D43', stop:'#C05D2A'}},
+  shadow:{type:String, default: '#EC7D4380'},
+  size:{type:String, default: 'h-20 w-20'}
+})
+</script>
+
+<style scoped>
+  .glass-icon-holder{
+   background: rgba(255, 255, 255, 0.40);
+    box-shadow: 0px 0px 4px 0px #FFF inset, 0px 4px 8px 0px #FFF inset;
+    backdrop-filter: blur(2px);
+  }
+  .icon-holder-bg{
+      box-shadow: 0px 0px 8px 0px rgba(255, 255, 255, 0.80) inset;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
-<script setup lang="ts"></script>
+<script setup>
+const main = ref(null)
+//watch(main, ()=>{
+//  console.log('Main found', main.value)
+  //main.value.addEventListener('mousemove', function(e) {
+  //    //const x = e.clientX - e.target.offsetLeft;
+  //    //const y = e.clientY - e.target.offsetTop;
+  //    //const backgroundStyle = `radial-gradient(circle at ${x}px ${y}px, rgba(255, 255, 255, 0.3) 10%, transparent 70%)`;
+  //    //main.value.style.backgroundImage = backgroundStyle;
+  //});
+//})
+</script>
 
 <template>
-  <main>
-    <nav class="flex justify-between py-8 z-50">
+  <main ref="main" class="overflow-x-visible">
+    <nav class="flex justify-between py-8">
       <!--add blobs-->
       <div class="absolute z-[-1] w-[300px] h-[300px] bg-amber-500 rotate-45 -inset-20 rounded-full blur-2xl opacity-80"></div>
       <div class="absolute z-[-1] w-[300px] h-[300px] bg-green-500 rotate-45 -top-20 inset-x-20 rounded-full blur-2xl opacity-40"></div>
@@ -22,7 +33,7 @@
 
     <div id="hero" class="max-w-4xl h-[70vh] mx-auto grid place-content-center text-center">
       <h1 class="text-7xl font-display">
-        <span class="mr-4 font-bold">Empowering</span>
+        <span class="mr-4 font-bold bg-clip-text text-transparent bg-gradient-to-br bg-gradient-[90deg] from-[#F6D58C]/30 via-[#FFB58B]/30 to-[#FF5353]">Empowering</span>
         <span class="font-thin">Innovation Through Software Excellence</span>
       </h1>
       <h2 class="text-4xl mt-20 font-normal">
@@ -30,14 +41,34 @@
       </h2>
 
       <div class="flex space-x-4 mt-12 justify-center">
-        <button>lkjhgfc</button>
-        <button>87654678</button>
+        <CtaButton>Let's Talk</CtaButton>
+        <button class="mesh-gradient-border py-2 px-3 rounded-full relative overflow-hidden">
+          <span class="relative z-10">Contact Us</span>
+        </button>
+
       </div>
 
       <!--decorate-->
       <div class="absolute z-[-1] w-[500px] h-[300px] bg-cyan-500 rotate-45 -bottom-32 -right-32 rounded-full blur-2xl opacity-50"></div>
       <div class="absolute z-[-1] w-[500px] h-[500px] bg-purple-500 rotate-45 -bottom-40 -right-56 rounded-full blur-2xl opacity-50"></div>
     </div>
+
+
+    <section class="mt-32 grid grid-cols-12 gap-4">
+      <div id="cards" class="col-span-4">
+        <div class="glass-card relative flex space-y-12 p-4">
+
+          <IconHolder size="h-12 w-12" />
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam autem, beatae commodi cupiditate dicta dolores ducimus esse, eveniet minus placeat quae ratione sapiente ullam vel! Enim recusandae repellendus sint!
+          </p>
+        </div>
+      </div>
+      <div class="col-span-8">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur eum facilis hic laborum natus non nostrum, numquam, perspiciatis quaerat quam quis quisquam, rerum voluptas? Enim officia, repellendus. Aperiam, omnis, vero!
+      </div>
+    </section>
 
     <slot />
   </main>
